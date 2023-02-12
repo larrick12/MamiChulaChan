0c66e5de24704bf52e83a6a73fd29ccde40e08ea
Temitayo Agboola <112914531+MamiChulaChan@users.noreply.github.com>
2023-02-12 21:40:54 +0100

    latest update

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 // Dom selector
 let menu = document.querySelector('.fa-bars');
 
-/** will check back on this to keep the code in one place */
-
 // small screen menu bars handler
 // menu.addEventListener('click', function () {
 //     let display = document.querySelector('#hamburger #dropdown');
