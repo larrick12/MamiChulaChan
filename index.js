3ac360fadc7b78ccd5abd40ec0f34790f057736e
Temitayo Agboola <112914531+MamiChulaChan@users.noreply.github.com>
2023-02-12 16:52:29 +0100

    Newly updated

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Dom selector
+let menu = document.querySelector('.fa-bars');
+
+/** will check back on this to keep the code in one place */
+
+// small screen menu bars handler
+// menu.addEventListener('click', function () {
+//     let display = document.querySelector('#hamburger #dropdown');
+//     display.remove()
+// });
